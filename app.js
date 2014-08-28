ada64099ede62d4d9086d8eda949b2c9d330673f
Domingo Garcia <domingo.garcia@pademobile.com>
2014-08-28 16:28:23 -0700

    Página /log

--- app.js
+++ app.js
@@ -114,6 +114,7 @@ app.get('/logout', function(req, res){
 app.get('/', ensureLoggedIn('/auth/google'), routes.index);
 // app.get('/', routes.index);
 app.get('/pagina1',  paginas.pagina1);
+app.get('/log',  paginas.pagina1);
 app.get('/pagina2', paginas.pagina2);
 app.get('/pagina3', paginas.pagina3);
 app.get('/pagina4', paginas.pagina4);
